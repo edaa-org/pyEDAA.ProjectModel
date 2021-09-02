ad2166d861160abd57ec5cd043b88aeacdf62d69
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-09-02 07:32:13 +0200

    Added instantiation tests.

---
+++ tests/unit/Project.py
@@ -0,0 +1,54 @@
+# =============================================================================
+#               _____ ____    _        _
+#   _ __  _   _| ____|  _ \  / \      / \
+#  | '_ \| | | |  _| | | | |/ _ \    / _ \
+#  | |_) | |_| | |___| |_| / ___ \  / ___ \
+#  | .__/ \__, |_____|____/_/   \_\/_/   \_\
+#  |_|    |___/
+# ==============================================================================
+# Authors:            Patrick Lehmann
+#
+# Python unittest:    Instantiation tests for the project model.
+#
+# License:
+# ==============================================================================
+# Copyright 2017-2021 Patrick Lehmann - Boetzingen, Germany
+# Copyright 2014-2016 Technische Universität Dresden - Germany
+#                     Chair of VLSI-Design, Diagnostics and Architecture
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ==============================================================================
+#
+from unittest import TestCase
+
+from pyEDAA.ProjectModel import Project
+
+
+if __name__ == "__main__":
+	print("ERROR: you called a testcase declaration file as an executable module.")
+	print("Use: 'python -m unitest <testcase module>'")
+	exit(1)
+
+
+class Instantiate(TestCase):
+	def test_Project(self):
+		project = Project("test")
+
+		self.assertIsNotNone(project)
+		self.assertEqual(project.Name, "test")
+		self.assertIsNone(project.RootDirectory)
+		self.assertIsNone(project.DefaultFileSet)
+		self.assertEqual(0, len(project.FileSets))
+		self.assertEqual(0, len(project.VHDLLibraries))
