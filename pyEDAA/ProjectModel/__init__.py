71ba9ee75f03688b0d99293580031bd4891385a7
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2022-01-16 14:55:55 +0100

    Bumped version to v0.4.3.

--- pyEDAA/ProjectModel/__init__.py
+++ pyEDAA/ProjectModel/__init__.py
@@ -34,7 +34,7 @@ __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
 __copyright__ = "2014-2022, Patrick Lehmann, Unai Martinez-Corral"
 __license__ =   "Apache License, Version 2.0"
-__version__ =   "0.4.2"
+__version__ =   "0.4.3"
 __keywords__ =  ["eda project", "model", "abstract", "xilinx", "vivado", "osvvm", "file set", "file group", "test bench", "test harness"]
 
 from os.path import relpath as path_relpath
