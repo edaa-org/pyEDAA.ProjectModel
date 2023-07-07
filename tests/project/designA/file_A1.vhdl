52c5a51efef76efe5a22a6d6b161c9bf898d93f2
Patrick Lehmann <Paebbels@gmail.com>
2023-07-07 07:49:31 +0200

    Add dependency scanning as a testcase.

--- tests/project/designA/file_A1.vhdl
+++ tests/project/designA/file_A1.vhdl
@@ -1,9 +1,13 @@
+library IEEE;
+use     IEEE.std_logic_1164.all;
 
-library libraryCommon;
-use     libraryCommon.P1.all;
+library libCommon;
+use     libCommon.P1.all;
 
 entity A1 is
-
+	port (
+		signal Clock : in std_logic
+	);
 end entity;
 
 architecture rtl of A1 is
@@ -11,4 +15,3 @@ architecture rtl of A1 is
 begin
 
 end architecture;
-
--- tests/project/designA/file_A2.vhdl
+++ tests/project/designA/file_A2.vhdl
@@ -1,16 +1,20 @@
+library IEEE;
+use     IEEE.std_logic_1164.all;
 
-library libraryCommon;
-use     libraryCommon.P2.all;
+library libCommon;
+use     libCommon.P2.all;
 
 entity A2 is
-
+	port (
+		signal Clock : in std_logic
+	);
 end entity;
 
 architecture rtl of A2 is
 
 begin
   a : entity work.A1
-    port (
-
+    port map (
+			Clock => Clock
     );
 end architecture;
